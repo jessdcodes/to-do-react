29fdc928e8c221460122d3b1b351709e0ba05247
jessdcodes <dionisiojessica15@gmail.com>
2024-02-19 23:42:26 +0800

    Remove unusable code

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,34 +1,40 @@
 import { useDispatch, useSelector } from "react-redux";
-import { addTask, removeTask, updateTask, updateTaskStatus, updateInputDisabled } from "../actions/listActions";
+import {
+  addTask,
+  removeTask,
+  updateTask,
+  updateTaskStatus,
+  updateInputDisabled,
+} from "../actions/listActions";
 import React, { useRef } from "react";
 import TodoDisplayButtons from "../components/TodoDisplayButtons";
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Input from '@material-ui/core/Input';
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Input from "@material-ui/core/Input";
 import Button from "@material-ui/core/Button";
 import { withStyles } from "@material-ui/core/styles";
-import DeleteIcon from '@mui/icons-material/Delete';
-import SendIcon from '@mui/icons-material/Send';
-import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from "@mui/icons-material/Delete";
+import SendIcon from "@mui/icons-material/Send";
+import EditIcon from "@mui/icons-material/Edit";
 import IconButton from "@material-ui/core/IconButton";
 import AddIcon from "@mui/icons-material/Add";
 
 const styles = {
   paper: {
     padding: 30,
-    margin: 'auto',
-    maxWidth: 400
+    margin: "auto",
+    maxWidth: 400,
   },
   row: {
-    flexBasis: '100%'
+    flexBasis: "100%",
   },
   taskInput: {
     color: "blue",
     marginRight: 8,
     "& .MuiInputBase-input.Mui-disabled": {
-      color: "black" // (default alpha is 0.38)
-    }
-  }
+      color: "black", // (default alpha is 0.38)
+    },
+  },
 };
 
 function Todo(props) {
@@ -38,7 +44,9 @@ function Todo(props) {
   const taskRef = useRef([]);
   const tickRef = useRef([]);
   const tasks = useSelector((state) => state.taskList.taskList);
-  const displaySetting = useSelector((state) => state.taskList.completedTasksFilter);
+  const displaySetting = useSelector(
+    (state) => state.taskList.completedTasksFilter
+  );
 
   function handleAdd() {
     dispatch(addTask(inputRef.current.value));
@@ -69,55 +77,68 @@ function Todo(props) {
       <Paper className={props.classes.paper}>
         <Grid container spacing={1} justifyContent="center">
           <Grid item xs md={12} className={props.classes.row}>
-            <TodoDisplayButtons/>
+            <TodoDisplayButtons />
           </Grid>
           <Grid item xs md={12} className={props.classes.row}>
-            <Input inputRef={inputRef} name="newTask" variant="filled" size="small"/>
+            <Input
+              inputRef={inputRef}
+              name="newTask"
+              variant="filled"
+              size="small"
+            />
             <IconButton onClick={handleAdd} color="primary">
               <AddIcon />
             </IconButton>
           </Grid>
           <Grid item xs md={12}>
             <ul>
-              {
-                tasks.filter(
-                  (task) => displaySetting!=null ? task.isCompleted === displaySetting : task
-                ).map((task) => {
-                return (
-                  <li key={task.id} className={props.classes.taskContainer}>
-                    <input 
-                      type="checkbox" 
-                      ref={(el) => (tickRef.current[task.id] = el)} 
-                      defaultChecked={task.isCompleted}
-                      onChange={() => handleTickChange(task.id)}/>
-                    <Input
-                      inputRef={(el) => (taskRef.current[task.id] = el)}
-                      disabled={task.isDisabled}
-                      className={props.classes.taskInput}
-                      defaultValue={task.taskName}
-                      style={{
-                          textDecoration: task.isCompleted ? "line-through" : "none"
+              {tasks
+                .filter((task) =>
+                  displaySetting != null
+                    ? task.isCompleted === displaySetting
+                    : task
+                )
+                .map((task) => {
+                  return (
+                    <li key={task.id} className={props.classes.taskContainer}>
+                      <input
+                        type="checkbox"
+                        ref={(el) => (tickRef.current[task.id] = el)}
+                        defaultChecked={task.isCompleted}
+                        onChange={() => handleTickChange(task.id)}
+                      />
+                      <Input
+                        inputRef={(el) => (taskRef.current[task.id] = el)}
+                        disabled={task.isDisabled}
+                        className={props.classes.taskInput}
+                        defaultValue={task.taskName}
+                        style={{
+                          textDecoration: task.isCompleted
+                            ? "line-through"
+                            : "none",
                         }}
-                    />
-                    <IconButton onClick={() => handleEdit(task.id)} variant="outlined">
-                      <EditIcon />
-                    </IconButton>
-                    <IconButton onClick={() => handleRemove(task.id)} variant="outlined">
-                      <DeleteIcon />
-                    </IconButton>
-                  </li>
-                );
-              })}
+                      />
+                      <IconButton
+                        onClick={() => handleEdit(task.id)}
+                        variant="outlined"
+                      >
+                        <EditIcon />
+                      </IconButton>
+                      <IconButton
+                        onClick={() => handleRemove(task.id)}
+                        variant="outlined"
+                      >
+                        <DeleteIcon />
+                      </IconButton>
+                    </li>
+                  );
+                })}
             </ul>
           </Grid>
         </Grid>
       </Paper>
-
-      
-      
     </div>
   );
 }
-
 
 export default withStyles(styles)(Todo);
